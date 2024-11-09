68370c6c81efd965301ab6731e0c0f4ee290d173
MShahbazh <msh23904t@gmail.com>
2024-11-10 00:53:24 +0500

    Update Problem 2.c

--- Assignment 02/Problem 2/Problem 2.c
+++ Assignment 02/Problem 2/Problem 2.c
@@ -3,22 +3,20 @@
 #include <string.h>
 int main() {
     int i,j,c;
-    char v[9999];
-    char s[9999];
-    int arr[9999]={0};
+    char v[9999],s[9999];
     printf("Enter the Slogans: ");
-     fgets(v, 9999, stdin); 
+    fgets(v, 9999, stdin); 
     char *x = strtok(v, ",");
     while (x != NULL) {
-        strcpy(s, x);
-    for(i=0;i<9999;i++){
-      j=s[i];
-      arr[j]++;
-    }
-    printf("For %s = {",s);
-    for(i=0;s[i]!='\0';i++){
-          if(s[i]!='0')
-        {
+            int arr[9999]={0};
+            strcpy(s, x);
+        for(i=0;i<9999;i++){
+            j=s[i];
+            arr[j]++;
+        }
+            printf("For %s = {",s);
+            for(i=0;s[i]!='\0';i++){
+        if(s[i]!='0'){
             printf("'%c': %d,",s[i],arr[s[i]]);
         }
         for(j=i+1;s[j]!='\0';j++){
